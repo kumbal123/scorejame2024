6f70bd1ecddd28ad3dcbaa346c8a368a98f7d667
kumbal123 <mmike.pxt@gmail.com>
2024-08-31 00:14:31 +0200

    Player movement + main scene adjustment

--- ourgame/src/PlayerCharacter.cs
+++ ourgame/src/PlayerCharacter.cs
@@ -5,8 +5,11 @@ using System;
 /// The class containing logic and controls for the player character.
 /// ... maybe change the Node type to something more specialized like CharacterBody..?
 /// </summary>
-public partial class PlayerCharacter : Node2D
+public partial class PlayerCharacter : CharacterBody2D
 {
+
+	public int Speed = 200;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -15,5 +18,33 @@ public partial class PlayerCharacter : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+		Velocity = Vector2.Zero;
+
+        // Process input for movement
+        if (Input.IsActionPressed("ui_up"))  // W key
+        {
+            Velocity += new Vector2(0, -1);
+        }
+        if (Input.IsActionPressed("ui_down"))  // S key
+        {
+            Velocity += new Vector2(0, 1);
+        }
+        if (Input.IsActionPressed("ui_left"))  // A key
+        {
+            Velocity += new Vector2(-1, 0);
+        }
+        if (Input.IsActionPressed("ui_right"))  // D key
+        {
+            Velocity += new Vector2(1, 0);
+        }
+
+        // Normalize the velocity to ensure consistent speed in diagonal movement
+        if (Velocity.Length() > 0)
+        {
+            Velocity = Velocity.Normalized() * Speed;
+        }
+
+        MoveAndSlide();
 	}
 }
