d4b76b43c88c6c0f36e411a881b5351657b146c1
Boopity <hydraccion@gmail.com>
2024-08-30 23:31:19 +0200

    Initial setup for PlayerCharacter node

---
+++ ourgame/src/PlayerCharacter.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+/// <summary>
+/// The class containing logic and controls for the player character.
+/// ... maybe change the Node type to something more specialized like CharacterBody..?
+/// </summary>
+public partial class PlayerCharacter : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
