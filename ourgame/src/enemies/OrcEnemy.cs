34d025be568872799e530eff86bd284e3ac1499a
luutranh <tr.huy.luu@gmail.com>
2024-09-01 16:17:41 +0200

    sounds a animace

--- ourgame/src/enemies/OrcEnemy.cs
+++ ourgame/src/enemies/OrcEnemy.cs
@@ -3,5 +3,17 @@ using System;
 
 public partial class OrcEnemy : EnemyBase
 {
-
+    private SoundPool _swordSound;
+    private SoundQueue _deathSound;
+    public override void loadSounds()
+	{
+		_swordSound = GetNode<SoundPool>("SwordSound");
+        _deathSound = GetNode<SoundQueue>("DeathSound");
+	}
+    public override void playAttackSound() {
+		_swordSound.PlayRandomSound();
+	}
+    public override void playDeathSound() {
+		_deathSound.PlaySound();
+	}
 }
